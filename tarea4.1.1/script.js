854edd73fbd8c88de19df470f5a779ceb40d95e6
Ismael <isidcas116@g.educaand.es>
2025-03-31 11:55:29 +0200

    ejercicio 3

--- tarea4.1.1/script.js
+++ tarea4.1.1/script.js
@@ -4,3 +4,6 @@ let var1 = -4;
 
 /* 2 Una variable palabra cuyo valor sea ”Hola Mundo”*/
 let palabra = "hola mundo";
+
+/* 3 Una variable booleana cuyo nombre sea respuesta e inicialmente con valor true*/
+var respuesta = new Boolean(true);
