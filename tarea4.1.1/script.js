11bb3c8a2a0f83c255fe2a0b7ee59d756c065a3a
Ismael <isidcas116@g.educaand.es>
2025-03-31 12:11:59 +0200

    ejercicio 5

--- tarea4.1.1/script.js
+++ tarea4.1.1/script.js
@@ -1,6 +1,6 @@
 /* 1 Una variable numero cuyo valor sea -4.*/
 
-let var1 = -4;
+let numero = -4;
 
 /* 2 Una variable palabra cuyo valor sea ”Hola Mundo”*/
 let palabra = "hola mundo";
@@ -8,5 +8,8 @@ let palabra = "hola mundo";
 /* 3 Una variable booleana cuyo nombre sea respuesta e inicialmente con valor true*/
 var respuesta = new Boolean(true);
 
-/* */ 
+/* 4 Crea una constante con nombre pi cuyo valor sea 3.14*/ 
 const pi = 3.14;
+
+/*5 Modifica el valor de la variable numero para que en lugar de -4 sea 54 pero sin tocar la parte de la declaración de la variable.*/
+numero=54;
